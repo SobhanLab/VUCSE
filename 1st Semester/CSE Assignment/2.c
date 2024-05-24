d2b6d67cfc74f6f3896f0f5c0b9ec6d01fb1653a
Abdus Sobhan <128705141+SobhanLab@users.noreply.github.com>
2024-05-24 20:26:31 +0600

    Update 2.c

--- 1st Semester/CSE Assignment/2.c
+++ 1st Semester/CSE Assignment/2.c
@@ -3,56 +3,47 @@
 #include <time.h>
 
 int randomint(int lower, int upper) {
-  return (rand() % (upper - lower + 1)) + lower;
+    return (rand() % (upper - lower + 1)) + lower;
 }
 
 void swap(char *first, char *second) {
-  char temp = *first;
-  *first = *second;
-  *second = temp;
+    char temp = *first;
+    *first = *second;
+    *second = temp;
 }
 
 int main() {
-  srand(time(NULL));
-  char special_chars[] = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-  int length;
-  printf("N = ");
-  scanf("%d", &length);
-  if (length < 6) {
-    length = 6;
-  }
-  char password[length + 1];
-  password[length] = '\0';
+    srand(time(NULL));
 
-  for (int i = 0; i < length; i++) {
-    switch (i) {
-    case 0:
-      password[i] = special_chars[randomint(0, sizeof(special_chars) - 2)];
-      break;
-    case 1:
-      password[i] = randomint('a', 'z');
-      break;
-    case 2:
-      password[i] = randomint('A', 'Z');
-      break;
-    case 3:
-      password[i] = randomint('A', 'Z');
-      break;
-    case 4:
-      password[i] = randomint('0', '9');
-      break;
-    case 5:
-      password[i] = randomint('0', '9');
+    char special_chars[] = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-    default:
-      password[i] = randomint(33, 126);
+    int length;
+    printf("N = ");
+    scanf("%d", &length);
+
+    if (length < 6) {
+        length = 6;
     }
-  }
 
-  for (int i = 0; i < length; i++) {
-    swap(&password[i], &password[randomint(0, length - 1)]);
-  }
+    char password[length + 1];
+    password[length] = '\0';
 
-  printf("Password = %s\n", password);
-  return 0;
+    password[0] = special_chars[randomint(0, sizeof(special_chars) - 2)];
+    password[1] = (char)randomint('a', 'z');
+    password[2] = (char)randomint('A', 'Z');
+    password[3] = (char)randomint('A', 'Z');
+    password[4] = (char)randomint('0', '9');
+    password[5] = (char)randomint('0', '9');
+
+    for (int i = 6; i < length; i++) {
+        password[i] = (char)randomint(33, 126);
+    }
+
+    for (int i = 0; i < length; i++) {
+        swap(&password[i], &password[randomint(0, length - 1)]);
+    }
+
+    printf("Password = %s\n", password);
+
+    return 0;
 }
